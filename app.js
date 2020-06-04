a6dd206ac9eda41b08428ffa9f970d5d4cc41e83
Dean Mullinger <dean@Deans-MacBook-Pro.local>
2020-06-04 22:28:56 +0100

    google oauth

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const passport = require('passport');
 const app = express(); 
 
+//Passport Config
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth'); 
+
 const port = process.env.PORT || 5000; 
+
+app.get('/', (req, res) =>{
+    res.send('it works'); 
+}); 
+
+app.use('/auth', auth); 
 
 app.listen(port, () =>{
     console.log(`Appliction started on port: ${port}`); 
 } )
 
-app.get('/', (req, res) =>{
-    res.send('it works')
-})
